c53e79f20e67eb1363d3906870f7424e136c4d2b
restumi <maulanaismael5@gmail.com>
2025-12-30 10:52:18 +0700

    add students CRUD api

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Classes\apiResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreStudentRequestApi;
+use App\Models\Hobby;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::with('phones', 'nisn', 'hobbies')->latest()->get();
+        return apiResponse::success($students, 'success get students');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'nisn' => 'required|string|unique:nisns,nisn',
+            'phones' => 'required|array|min:1',
+            'phones.*' => 'required|string|max:20',
+        ],[
+            'name.required' => 'name is required',
+            'nisn.required' => 'nisn is required',
+            'phones.required' => 'phones is required',
+            'phones.*.required' => 'phones is required bre'
+        ]);
+
+        $student = Student::create(['name' => $request->name]);
+
+        $student->nisn()->create(['nisn' => $request->nisn]);
+
+        foreach($request->phones as $number){
+            $student->phones()->create(['number' => $number]);
+        }
+
+        if($request->hobbies){
+            $student->hobbies()->attach($request->hobbies);
+        }
+
+        return apiResponse::success($student, "Student created", 201);
+    }
+
+    public function update(Request $request, Student $student)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'nisn' => 'required|string|unique:nisns,nisn' . $student->nisn?->id,
+            'phones' => 'required|array|min:1',
+            'phones.*' => 'required|string|max:20'
+        ]);
+
+        $student->update(['name' => $request->name]);
+
+        $student->nisn()->update(['nisn' => $request->nisn]);
+
+        $student->phones()->delete();
+        foreach($request->phones as $number){
+            $student->phones()->create(['number' => $number]);
+        }
+
+        $student->hobbies()->sync($request->hobbies ?? []);
+
+        return apiResponse::success($student, "Student updated");
+    }
+
+    public function destroy(Student $student)
+    {
+        $student->delete();
+        return apiResponse::success('', "Student deleted");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,20 @@ use App\Classes\apiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\HobbyController;
+use App\Http\Controllers\Api\StudentController;
 
 Route::get('/test', function(){
     return apiResponse::success();
 });
 
+// Hobbies
 Route::get('/hobbies', [HobbyController::class, 'index']);
 Route::post('/hobbies', [HobbyController::class, 'store']);
 Route::put('/hobbies/{hobby}', [HobbyController::class, 'update']);
 Route::delete('/hobbies/{hobby}', [HobbyController::class, 'destroy']);
+
+// Students
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{student}', [StudentController::class, 'update']);
+Route::delete('/students/{student}', [StudentController::class, 'destroy']);
